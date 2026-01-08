28e0b5e708f336474e6e8eca84684211c7c6a61c
GoluScriptMage <totalgaming1235@gmail.com>
2026-01-08 12:58:19 +0530

    Frontend structure modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "@/components/navbar";
-import ShowNavbar from "./show-navbar";
+import ShowNavbar from "./(home)/show-navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
